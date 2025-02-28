43b62b1ef7e82c6145f138484a5c167fd8689214
Israa-Alzein <israaalzein09@gmail.com>
2025-02-28 14:37:06 +0200

    created methods for Car that override Vehicle methods

--- car_rental_service.py
+++ car_rental_service.py
@@ -45,3 +45,9 @@ class Car(Vehicle):
     def __init__(self, brand, model, year, rental_price_per_day, seating_capacity):
         super().__init__(brand, model, year, rental_price_per_day)  
         self.seating_capacity = seating_capacity
+
+    def display_info(self):
+        print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity} ,Rental Price: ${self.rental_price_per_day}/day")
+
+    def calculate_rental_cost(self, days):
+        return super().calculate_rental_cost(days)
