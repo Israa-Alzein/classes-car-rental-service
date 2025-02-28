409b960b7bd1362b7572df7115b971e185938b2b
Israa-Alzein <israaalzein09@gmail.com>
2025-02-28 21:26:27 +0200

    displayed the car rental view to interact with the user

--- car_rental_service.py
+++ car_rental_service.py
@@ -75,22 +75,103 @@ class Bike(Vehicle):
         return super().calculate_rental_cost(days)
 
 
+
+def prompt():
+    print()
+    print("=============== Bob's Car Rental Service =================")
+    print("Select from the menu below:")
+    print("1 ==> To create a new car")
+    print("2 ==> To create a new bike")
+    print("3 ==> To view car info")
+    print("4 ==> To view bike info")
+    print("5 ==> To calculate the total rent cost of car for selected number of days")
+    print("6 ==> To calculate the total rent cost of bike for selected number of days")
+    print("7 ==> To update the cost per day of the car")
+    print("8 ==> To update the cost per day of the bike")
+    print("9 ==> To exit")
+
+    action = int(input("Choice: "))
+    print("==========================================================")
+    print()
+
+    return action
+
+def createCar():
+    brand = input("Enter the car brand: ")
+    model = input("Enter the car model: ")
+    year = int(input("Enter the year of the car: "))
+    rental_price_per_day = float(input("Enter the rent price of the car per day: "))
+    seating_capacity = int(input("Enter the seating capacity of the car: "))
+    car = Car(brand, model, year, rental_price_per_day, seating_capacity)
+    return car
+
+def createBike():
+    brand = input("Enter the bike brand: ")
+    model = input("Enter the bike model: ")
+    year = int(input("Enter the year of the bike: "))
+    rental_price_per_day = float(input("Enter the rent price of the bike per day: "))
+    engine_capacity = input("Enter the bike's engine capacty: ")
+    bike = Bike(brand, model, year, rental_price_per_day, engine_capacity)
+    return bike
+
 def show_vehicle_info(vehicle):
     vehicle.display_info()
 
+def calculate_rent_cost(vehicle):
+    days = int(input("For how many days you want to rent the vehicle? : "))
+    vehicle.calculate_rental_cost(days)
 
-c1 = Car("Toyota", "Corolla", 2020, 50, 5)
-b1 = Bike("Yamaha", "R1", 2019, 30,"998cc")
-c1.display_info()
-b1.display_info()
+def update_cost_per_day(vehicle):
+    new_cost = float(input("Enter the new cost: "))
+    vehicle.rental_price_per_day = new_cost
+    print(f"Updated rental price for {vehicle.brand} {vehicle.model}: ${vehicle.rental_price_per_day}/day")
 
-print()
 
-c1.calculate_rental_cost(3)
-b1.calculate_rental_cost(5)
+# ========= Main Program ==========
 
-print()
 
-c1.rental_price_per_day = 55
-print(f"Updated rental price for Toyota Corolla: ${c1.rental_price_per_day}/day")
+action = 0
+previousAction = None
+car = None
+bike = None
 
+while action != 9:
+    if previousAction != None:
+        action = previousAction
+        previousAction = None
+        print(f"Doing action {action} from previous iteration")
+    else:
+        action = prompt()
+
+    if car == None and action != 1 and action != 9 and (action == 3 or action == 5 or action == 7):
+        print("Create a car first:")
+        previousAction = action
+        action = 1
+    elif bike == None and action != 2 and action != 9 and (action == 4 or action == 6 or action == 8):
+        print("Create a bike first:")
+        previousAction = action
+        action = 2
+
+    if action == 1:
+        car = createCar()
+        print()
+    elif action == 2:
+        bike = createBike()
+        print()
+    elif action == 3:
+        show_vehicle_info(car)  
+        print()
+    elif action == 4:
+        show_vehicle_info(bike)
+        print()
+    elif action == 5:
+        calculate_rent_cost(car)
+        print()
+    elif action == 6:
+        calculate_rent_cost(bike)
+    elif action == 7:
+        update_cost_per_day(car)
+    elif action == 8:
+        update_cost_per_day(bike)
+    elif action == 9:
+        print("Program Exit")
